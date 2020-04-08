82e21a2e9a8761dab1511bb7680315e7e6e1c722
Tomek <owerko@gmail.com>
2020-04-08 21:18:53 +0200

    DF to list

--- bathymetry/sonar.py
+++ bathymetry/sonar.py
@@ -1,3 +1,7 @@
 import pandas as pd
 data = pd.read_csv('Chart 6_5_16 [0].csv')
 print(data.head())
+depth = data['Depth'].to_list()
+time = data['TimeOffset[ms]'].to_list()
+print(depth)
+print(time)
